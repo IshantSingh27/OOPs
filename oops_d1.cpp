618615831b5ab58cf2796416f2d611d95ced33f7
IshantSingh27 <ishantsingh0602@gmail.com>
2025-07-10 19:37:38 +0530

    d1

--- oops_d1.cpp
+++ oops_d1.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 class Employee{
-    private:
+    private: //can be accessed in class only
     int a , b , c;
-    public:
+    public: //can be accessed anywhere
     int d , e;
-    void set(int a , int b , int c);
+    void set(int a , int b , int c); // gives assurance that you will find it later.
     void get(){
         cout<<"value of a is : "<<a<<endl;
         cout<<"value of b is : "<<b<<endl;
@@ -15,18 +15,17 @@ class Employee{
         cout<<"value of e is : "<<e<<endl;
     }
 };
-
 void Employee :: set(int a1 , int b1 , int c1){
-    a = a1;
+    a = a1; //This function is Declared out of the class.
     b = b1;
     c = c1;
 }
 
 int main() {
-    Employee ish;
-    ish.d = 7;
+    Employee ish; //object of class
+    ish.d = 7; //public can be accessed
     ish.e = 18;
-    ish.set(4 , 5 , 6);
+    ish.set(4 , 5 , 6); //private using function
     ish.get();
 
     return 0;
