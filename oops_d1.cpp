3fed714fcfd8d2ad53aa7a46d38111266724ea27
IshantSingh27 <ishantsingh0602@gmail.com>
2025-07-10 19:39:33 +0530

    d1

--- oops_d1.cpp
+++ oops_d1.cpp
@@ -1,6 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 class Employee{
     private: //can be accessed in class only
     int a , b , c;
@@ -20,13 +19,11 @@ void Employee :: set(int a1 , int b1 , int c1){
     b = b1;
     c = c1;
 }
-
 int main() {
     Employee ish; //object of class
     ish.d = 7; //public can be accessed
     ish.e = 18;
     ish.set(4 , 5 , 6); //private using function
     ish.get();
-
     return 0;
 }
